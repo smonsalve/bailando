3258769611801fd7dd3932d86926065c62a696ff
horus <smonsalve@gmail.com>
2013-10-03 12:06:14 -0500

    first Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bailando::Application.config.secret_key_base = '9ca22ad393ef3a8e2a2db7cb896f5bd3acc4427a437dcdc9a892826a3548f3b012617623d5089a0d3bd979df5d086d9bf650b217d0682206473b9d06f2a4b21a'
